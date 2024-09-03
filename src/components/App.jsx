8cb60711ec4fced73ab8c49cc9af26b4c7af8f2d
jakanoh <jakanoh17@gmail.com>
2024-09-03 05:39:00 -0400

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,114 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Ducks from "./Ducks";
 import Login from "./Login";
 import MyProfile from "./MyProfile";
 import Register from "./Register";
+import ProtectedRoute from "./ProtectedRoute";
 import "./styles/App.css";
+import * as auth from "../utils/auth";
+import { getToken, setToken } from "../utils/token";
 
 function App() {
+  const [userData, setUserData] = useState({ username: "", email: "" });
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleRegistration = ({
+    username,
+    email,
+    password,
+    confirmPassword,
+  }) => {
+    if (password === confirmPassword) {
+      auth
+        .register(username, password, email)
+        .then(() => {
+          // Navigate user to login page.
+          navigate("/login");
+        })
+        .catch(console.error);
+    }
+  };
+
+  // handleLogin accepts one parameter: an object with two properties.
+  const handleLogin = ({ username, password }) => {
+    // If username or password empty, return without sending a request.
+    if (!username || !password) {
+      return;
+    }
+
+    // We pass the username and password as positional arguments. The
+    // authorize function is set up to rename `username` to `identifier`
+    // before sending a request to the server, because that is what the
+    // API is expecting.
+    auth
+      .authorize(username, password)
+      .then((data) => {
+        // Verify that a jwt is included before logging the user in.
+        if (data.jwt) {
+          setToken(data.jwt);
+          setUserData(data.user); // save user's data to state
+          setIsLoggedIn(true); // log the user in
+          navigate("/ducks"); // send them to /ducks
+        }
+      })
+      .catch(console.error);
+  };
+
+  useEffect(() => {
+    const jwt = getToken();
+
+    if (!jwt) {
+      return;
+    }
+
+    // TODO - handle JWT
+  }, []);
+
   return (
     <Routes>
-      <Route path="/ducks" element={<Ducks />} />
-      <Route path="/my-profile" element={<MyProfile />} />
+      <Route
+        path="/ducks"
+        element={
+          <ProtectedRoute isLoggedIn={isLoggedIn}>
+            <Ducks />{" "}
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/my-profile"
+        element={
+          <ProtectedRoute isLoggedIn={isLoggedIn}>
+            <MyProfile userData={userData} />{" "}
+          </ProtectedRoute>
+        }
+      />
       <Route
         path="/login"
         element={
           <div className="loginContainer">
-            <Login />
+            <Login handleLogin={handleLogin} />
           </div>
+        }
+      />
+      <Route
+        path="*"
+        element={
+          isLoggedIn ? (
+            <Navigate to={"/ducks"} replace />
+          ) : (
+            //The "replace" keeps the browser history clean by replacing the current entry in the history stack instead of adding a new one
+            <Navigate to={"/login"} replace />
+          )
         }
       />
       <Route
         path="/register"
         element={
           <div className="registerContainer">
-            <Register />
+            <Register handleRegistration={handleRegistration} />
           </div>
         }
       />
